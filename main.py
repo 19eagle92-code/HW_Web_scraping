d4b8b689c044ae41b195c8c6cbcb6cc8f080a618
Kirill Gmirya <Gmiryk_Kirill@mail.ru>
2026-01-30 18:23:53 +0300

    tring simple example

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello, Python project!")
+import requests
+import re
+
+URL = "https://habr.com/ru/articles/"
+
+response = requests.get(URL)
+# print(response.request.headers)
+
+# Определяем список ключевых слов:
+KEYWORDS = ["дизайн", "фото", "web", "python"]
+
+markup = response.text
+# print(markup)
+
+matches = re.findall(r"дизайн", markup)
+print(matches)
 
 
-if __name__ == "__main__":
-    main()
+# for word in KEYWORDS:
+#     matches = re.findall(word, markup)
+#     print(f" для слова '{word}' найдено - {len(matches)} совпадений")
